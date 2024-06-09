001d379ee90235b6adfaf9943364be9ad6ca651e
SATYAKI SAHA <satyaki01saha@gmail.com>
2024-06-10 01:16:32 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,18 +1,21 @@
 var k = document.getElementById("yes");
 var y = document.getElementById("no");
 var img = document.getElementById("im");
+document.getElementById("aud1").style.display="none";
+
 img.style.height="300px";
 img.style.width="300px";
 var c=0;
 if(!c){
-setTimeout(function() { alert("Why So Much Time"); }, 10000);
+setTimeout(function() {if(!c) {alert("Why So Much Time");} }, 10000);
 }
 function accha()
 {
     c=1;
     alert("Good Decision!!");
     img.src="couldbeus.png";
-    
+    aud.src="reelaudio.mp3";
+    document.getElementById("aud1").style.display="block";
 }
 img.style.zIndex = 0;
 y.style.zIndex = 9999;
