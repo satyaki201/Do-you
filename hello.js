e4aca89894d4449b5201bcb07c1b59bd4aee15ce
SATYAKI SAHA <satyaki01saha@gmail.com>
2024-06-09 16:48:22 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,15 @@
+var k = document.getElementById("yes");
+var y = document.getElementById("no");
+var img = document.getElementById("im");
+
+img.style.zIndex = 0;
+y.style.zIndex = 9999;
+
+function jump() {
+    var a = Math.random() * 200;
+    var b = Math.random() * 200 ;
+
+    console.log(a, b);
+    y.style.left = `${a}px`;
+    y.style.top = `${b}px`;
+}
