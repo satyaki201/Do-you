bbb63ff2c4223999fefa776e0b60e547444b8958
SATYAKI SAHA <satyaki01saha@gmail.com>
2024-06-09 22:49:38 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -10,8 +10,8 @@ function accha()
     c=1;
     alert("Good Decision!!");
     img.src="couldbeus.png";
-    img.height="100px";
-    img.width="100px";
+    img.style.height="100px";
+    img.style.width="100px";
 }
 img.style.zIndex = 0;
 y.style.zIndex = 9999;
